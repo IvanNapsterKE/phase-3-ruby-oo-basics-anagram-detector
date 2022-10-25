15b72ad1a2a269ecb92a95059f49b2006e187660
IvanNapsterKE <nguyoivan@gmail.com>
2022-10-25 15:18:10 +0300

    commit tree 77d43cfb593eb7bed6935608b37c1b28c6eae684

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def match(str_array)
+        str_array.filter {|name| 
+            name.chars.sort == self.name.chars.sort
+        }
+    end
+
+end
